7197612a3a2a6e57d0c12c910aa809c602a6b671
JoseMiguelRegoNeves <1200901@isep.ipp.pt>
2022-01-03 11:01:47 +0000

    imports gerais

--- src/LAPR1TurmaDEFGrupo03.java
+++ src/LAPR1TurmaDEFGrupo03.java
@@ -1,8 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class LAPR1TurmaDEFGrupo03 {
     public static void main(String[] args) {
-        System.out.println("catarina");
-        System.out.println("Miguel");
-        // joana
-        // catarina
+
     }
 }
