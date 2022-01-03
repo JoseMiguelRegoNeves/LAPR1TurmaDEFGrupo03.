f457debde7d174e86fd26c296010950582bc2eb7
JoanaSofia <1211148@isep.ipp.pt>
2022-01-03 10:46:23 +0000

    hghnfgh

--- src/LAPR1TurmaDEFGrupo03.java
+++ src/LAPR1TurmaDEFGrupo03.java
@@ -1,7 +1,4 @@
 public class LAPR1TurmaDEFGrupo03 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        System.out.println("Adeus!");
-        System.out.println("Miguel!");
     }
 }
