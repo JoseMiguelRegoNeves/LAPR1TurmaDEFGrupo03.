bb1257dfd94671dd763b801198964f12b05bf199
JoseMiguelRegoNeves <1200901@isep.ipp.pt>
2022-01-03 10:42:27 +0000

    Teste Miguel

--- src/LAPR1TurmaDEFGrupo03.java
+++ src/LAPR1TurmaDEFGrupo03.java
@@ -2,5 +2,6 @@ public class LAPR1TurmaDEFGrupo03 {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Adeus!");
+        System.out.println("Miguel!");
     }
 }
