bf321f8a02e12eac1e9e62887fd573de76d696a1
JoseMiguelRegoNeves <1200901@isep.ipp.pt>
2022-01-04 15:47:18 +0000

    Correção de Bugs

--- src/LAPR1TurmaDEFGrupo03.java
+++ src/LAPR1TurmaDEFGrupo03.java
@@ -6,25 +6,27 @@ import java.io.FileNotFoundException;
 public class LAPR1TurmaDEFGrupo03 {
 
     public static void main(String[] args) throws FileNotFoundException{
-        try{
-            r=args[1];
-            di=args[3];
-            df=args[5];
-            in=args[6];
-            out=args[7];
-            int[][] matriz= Scann();
+        int[][] matriz = new int[5][5];
+        try{                                                                                                                //CATCH CORRE MESMO QUANDO O TRY ESTÁ EXECUTÁVEL
+            String r = args[1];
+            String di = args[3];
+            String df = args[5];
+            String in = args[6];
+            String out = args[7];
+            matriz= Scann();
         }catch (Exception e) {
             System.out.println("Modo Iterativo.");
-            int[][] matriz= Scann();
+            matriz= Scann();
+
         }
     }
 
-    public static final String delimiter = ",";                                                                          //AJUDA!
+    //public static final String delimiter = ",";                                                                           //AJUDA! NÃO ESTÁ A FUNCIONAR
     public static int[][] Scann() throws FileNotFoundException{
-        Scanner sc = new Scanner(new File("nome_do_ficheiro"));
-        int[][] matrix;
-        for (int i = 0; i < ; i++) {
-            for (int j = 0; j < ; j++) {
+        Scanner sc = new Scanner(new File("nome_do_ficheiro.csv"));
+        int[][] matrix = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
                 matrix[i][j] = Integer.parseInt(sc.next());
             }
         }
